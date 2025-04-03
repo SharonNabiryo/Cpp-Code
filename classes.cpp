d473fb3a8204c81e2b9f218b920913210b81672a
Sharon N Nabiryo <sharongless37@gmail.com>
2025-04-02 22:39:37 -0400

    classes

---
+++ classes.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+enum Month {
+    None, Jan, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec};
+
+class DOB {
+    public:
+    Month month;
+    int day;
+    int year;
+
+    DOB(){
+        month = None;
+    day = 0;
+    year = 0;
+    }
+
+    void setMonth(Month m){
+        month = m;
+    }
+    void setDay(int d){
+        day = d;
+    }
+    void setYear(int y) {
+        year = y;
+    }
+    string getDate(){
+        return to_string(month) + "/" + to_string(day) + "/" + to_string(year);
+    }
+};
+
+int main(){
+    
+    DOB dob;
+
+    dob.setMonth(Mar);
+    dob.setDay(14);
+    dob.setYear(2012);
+
+    cout<< dob.getDate() << endl;
+
+    return 0;
+}
